a9a87a4b6f72c994959b492e9b94563ae957f6db
DTS <Cstaton_2216@Icloud.com>
2025-08-13 13:02:43 -0400

    Initial Commit

---
+++ DTS App/DTS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DTS_AppApp.swift
+//  DTS App
+//
+//  Created by Chandler Staton on 8/13/25.
+//
+
+import SwiftUI
+
+@main
+struct DTS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
